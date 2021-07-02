cefbd88084f277e4d7e0234b9cc3fe6218a2ef6a
stst5821 <stst5821@gmail.com>
2021-07-02 21:31:56 +0900

    inputからlaravel通してDBに登録できるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <form @submit.prevent>
+      <label
+        >入力：
+        <input
+          v-model="name"
+          placeholder="入力ください"
+          type="text"
+          name="entry"
+          @keyup.enter="submit"
+        />
+      </label>
+      <button type="submit" @click="submit">送信</button>
+    </form>
+
     <div v-for="result in results" :key="result.id">
       {{ result.id }}
       {{ result.name }}
@@ -15,11 +29,12 @@ export default {
   data() {
     return {
       results: '',
+      name: '',
     }
   },
   mounted() {
     axios
-      .get('http://localhost/test1')
+      .get('http://localhost/api/test1')
       .then((response) => {
         this.results = response.data.result
         console.log(this.results)
@@ -29,5 +44,25 @@ export default {
         this.result = 'ERROR'
       })
   },
+  methods: {
+    submit() {
+      // const submitParams = new FormData()
+      // submitParams.append('entry', this.name)
+      const CORS_PROXY = 'http://localhost/'
+      const name = this.name
+      console.log(name)
+      axios.post(CORS_PROXY + 'api/store', { name })
+    },
+  },
+  // addData() {
+  //   axios
+  //     .post('http://localhost/store', {
+  //       name: '送る名前',
+  //     })
+  //     .then(function (response) {
+  //       // this.name = response.name
+  //       console.log(response.data)
+  //     })
+  // },
 }
 </script>
