24d8090ef7a2dd1291d6b4108f7ec48cd092684b
stst5821 <stst5821@gmail.com>
2021-07-08 11:09:11 +0900

    edit画面にデータを表示させるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <form @submit.prevent>
       <label
-        >入力：
+        >名前：
         <input
           v-model="name"
           placeholder="入力ください"
@@ -11,15 +11,41 @@
           @keyup.enter="submit"
         />
       </label>
-      <button type="submit" @click="submit">送信</button>
+      <br />
+      <label
+        >本文：
+        <input
+          v-model="body"
+          placeholder="入力ください"
+          type="text"
+          name="entry"
+          @keyup.enter="submit"
+        />
+      </label>
+      <button
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-1
+          px-2
+          rounded
+        "
+        type="submit"
+        @click="submit"
+      >
+        送信
+      </button>
     </form>
     <!-- <div>
       <p>{{ $store.state.results }}</p>
     </div> -->
     <ul>
       <li v-for="result in results" :key="result.id">
-        <!-- <li v-for="result in $store.state.results" :key="result.id"> -->
-        id:{{ result.id }} | name:{{ result.name }} | 本文:{{ result.body }}
+        id:
+        <nuxt-link :to="'/posts/' + result.id">{{ result.id }}</nuxt-link>
+        | name:{{ result.name }} | 本文:{{ result.body }}
         <button @click="remove(result.id)">X</button>
       </li>
     </ul>
@@ -35,6 +61,7 @@ export default {
     return {
       results: '',
       name: '',
+      body: '',
     }
   },
   mounted() {
@@ -54,12 +81,14 @@ export default {
     submit() {
       const CORS_PROXY = 'http://localhost/'
       // DBへの登録が完了したら、getでDBからデータを取ってきて追加したデータを画面に表示させる。
-      axios.post(CORS_PROXY + 'api/store', { name: this.name }).then(() => {
-        axios.get(CORS_PROXY + 'api/test1').then((response) => {
-          this.results = response.data.result
-          console.log(this.results)
+      axios
+        .post(CORS_PROXY + 'api/store', { name: this.name, body: this.body })
+        .then(() => {
+          axios.get(CORS_PROXY + 'api/test1').then((response) => {
+            this.results = response.data.result
+            console.log(this.results)
+          })
         })
-      })
     },
     remove(id) {
       const CORS_PROXY = 'http://localhost/'
---
+++ pages/posts/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="result in results" :key="result.id">
+      id:{{ result.id }} | name:{{ result.name }} | 本文:{{ result.body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const res = await context.$axios.$get(
+      `http://localhost/api/edit/${context.params.id}`
+    )
+    console.log(res)
+    return {
+      results: res,
+    }
+  },
+  data() {
+    return {
+      results: '',
+      name: '',
+      body: '',
+    }
+  },
+}
+</script>
